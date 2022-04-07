fef2d7346da74e7752a752f78d192afd55a6bec6
aleniso <62444417+aleniso@users.noreply.github.com>
2022-04-06 20:13:56 -0500

    Update quiz_monedas.py

--- quiz_monedas.py
+++ quiz_monedas.py
@@ -50,27 +50,27 @@ def solve_quiz():
 		sailor_count = 1
 		current_sailor_list = []
 		left_coins = n
-		r = remainder(left_coins,3)
-		if ( r[0] ==  1):
-			sailor_coins = r[1]
+		r, q = remainder(left_coins,3)
+		if ( r ==  1):
+			sailor_coins = q
 			current_sailor_list.append(Register("Marinero",left_coins,sailor_count,sailor_coins))
 			left_coins = left_coins - sailor_coins - 1
 			sailor_count = sailor_count + 1
-			r = remainder(left_coins,3)
-			if (r[0] ==  1):
-				sailor_coins = r[1]
+			r, q = remainder(left_coins,3)
+			if (r ==  1):
+				sailor_coins = q
 				current_sailor_list.append(Register("Marinero",left_coins,sailor_count,sailor_coins))
 				left_coins = left_coins - sailor_coins - 1
 				sailor_count = sailor_count + 1
-				r = remainder(left_coins,3)
-				if (r[0] ==  1):
-					sailor_coins = r[1]
+				r, q = remainder(left_coins,3)
+				if (r ==  1):
+					sailor_coins = q
 					current_sailor_list.append(Register("Marinero",left_coins,sailor_count,sailor_coins))
 					left_coins = left_coins - sailor_coins - 1
 					sailor_count = sailor_count + 1
 					r = remainder(left_coins,3)
-					if (r[0] ==  1):
-						coins_to_be_distribuited = r[1]
+					if (r ==  1):
+						coins_to_be_distribuited = q
 						sailor_coins = 1
 						current_sailor_list.append(Register("Almojarife",left_coins,sailor_count,sailor_coins))
 						for count in range(len(current_sailor_list) - 1):
